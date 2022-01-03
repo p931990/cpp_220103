19f1dc2fd63fc849b61c9e2091334ca2cfc1ca28
Chansik Yun <chansigi@gmail.com>
2022-01-03 13:34:37 +0900

    .

--- 220103/1.cpp
+++ 220103/1.cpp
@@ -1,11 +1,36 @@
+
+
+// 컴파일 방법: g++ 1.cpp
+//           clang++ 1.cpp
+// 실행 방법: ./a.out
+//          ./a.exe
+
+// 1. C++ 확장자
+//    [.cpp], .cc, .cxx, .cp, .c++
+
+// 2. C++ 컴파일러
+//   1) clang++
+//   2) g++
+//   3) Microsoft C++ Compiler
+
+// 3. 표준(Standard)
+//  C++ 1.0 - 98/03
+//  C++ 2.0 - 11/14: Modern C++
+//  C++ 3.0 - 17/20
+
+// C++ vs C
+/*
 #include <stdio.h>
 
 int main()
 {
   printf("Hello, C");
 }
+*/
 
-// 컴파일 방법: g++ 1.cpp
-//           clang++ 1.cpp
-// 실행 방법: ./a.out
-//          ./a.exe
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, C++" << std::endl;
+}
