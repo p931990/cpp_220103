1391bc2bb6082bd21bfb35fc306db8429eb5eb38
Chansik Yun <chansigi@gmail.com>
2022-01-03 14:41:52 +0900

    .

--- 220103/2_iostream.cpp
+++ 220103/2_iostream.cpp
@@ -23,6 +23,12 @@ int main()
 {
   int n = 0;
   cin >> n;
+  // scanf("%d", &n);
+  // 1. 정수 타입을 지정할 필요가 없다.
+  // 2. 주소를 보내지 않아도, 값이 변경된다.
+  // 3. std::cout, std::cin
+  //    정체는 무엇인가?
+  // 4. std::endl 정체는 무엇인가?
 
   // cout << "out: " << n << std::endl;
   cout << "out: " << n;
