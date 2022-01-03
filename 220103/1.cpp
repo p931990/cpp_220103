b7fccdf1b778c69679eabea5263082766ab3f44c
Chansik Yun <chansigi@gmail.com>
2022-01-03 13:40:13 +0900

    .

--- 220103/1.cpp
+++ 220103/1.cpp
@@ -29,8 +29,13 @@ int main()
 */
 
 #include <iostream>
+// C++ 표준 헤더: 헤더 파일의 확장자가 없는 경우가 많습니다.
+
+// #include <stdio.h> // C style 헤더, C++에서도 사용할 수 있습니다.
+#include <cstdio> // C++ 에서는 앞에 c를 붙이고, .h를 제거한 형태를 권장합니다.
 
 int main()
 {
+  printf("Hello, C\n");
   std::cout << "Hello, C++" << std::endl;
 }
