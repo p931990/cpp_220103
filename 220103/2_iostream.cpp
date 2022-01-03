01dff988367a4aeae76fe7ccf684039f8928d888
Chansik Yun <chansigi@gmail.com>
2022-01-03 14:31:05 +0900

    .

---
+++ 220103/2_iostream.cpp
@@ -0,0 +1,30 @@
+// 2_iostream.cpp
+// > C++ 표준 입출력
+// 1. C언어에서는 표준 입출력을 위해 대표적으로 사용하는 함수
+//   : printf, scanf
+
+/*
+#include <stdio.h>
+
+int main()
+{
+  int n = 0;
+  scanf("%d", &n);
+
+  printf("out: %d\n", n);
+}
+*/
+
+// C++에서 제공하는 표준 입출력
+#include <iostream>
+using namespace std; // !!!
+
+int main()
+{
+  int n = 0;
+  cin >> n;
+
+  // cout << "out: " << n << std::endl;
+  cout << "out: ";
+  cout << std::endl;
+}
