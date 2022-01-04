e4c973324a01946d55e339705c69122ede636a13
Chansik Yun <chansigi@gmail.com>
2022-01-04 16:25:04 +0900

    .

--- 220104/10_OOP2.cpp
+++ 220104/10_OOP2.cpp
@@ -32,6 +32,7 @@ int main()
 }
 */
 
+/*
 // 2. Stack 타입을 설계해봅시다.
 struct Stack
 {
@@ -63,4 +64,51 @@ int main()
 
   cout << pop(&s1) << endl;
   cout << pop(&s2) << endl;
+}
+*/
+
+// 3. 상태를 나타내는 데이터와 상태를 조작하는 함수를 묶자.
+struct Stack
+{
+  // 멤버 변수(데이터)
+  int buff[10];
+  int idx;
+
+  // 멤버 함수
+  // - 멤버 데이터에 접근하는 함수
+  // > 명시적인 함수 인자로 Stack*을 받을 필요가 없습니다.
+  void init()
+  {
+    idx = 0;
+  }
+
+  void push(int v)
+  {
+    buff[idx++] = v;
+  }
+
+  int pop()
+  {
+    return buff[--idx];
+  }
+};
+
+Stack s1;
+int main()
+{
+  Stack s2;
+
+  // 초기화가 필요합니다.
+  // s1.idx = 0;
+  s1.init();
+  // s2.idx = 0;
+  s2.init();
+
+  s1.push(10);
+  s2.push(20);
+  // push(&s1, 10);
+  // push(&s2, 20);
+
+  cout << s1.pop() << endl;
+  cout << s2.pop() << endl;
 }
