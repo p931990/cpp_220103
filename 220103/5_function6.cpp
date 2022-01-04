0dd6db3140b61b0daad0dea41cf89a54535bbdbf
Chansik Yun <chansigi@gmail.com>
2022-01-04 10:59:47 +0900

    .

--- 220103/5_function6.cpp
+++ 220103/5_function6.cpp
@@ -6,7 +6,11 @@ using namespace std;
 // => 함수의 주소를 참조하는 포인터입니다.
 // => 함수 포인터를 통해 함수를 호출할 수 있습니다.
 
-int add(int a, int b) { return a + b; }
+// 함수의 타입은 함수의 파라미터와 반환타입에 의해 결정된다.
+//  => 함수의 파라미터와 반환 타입이 동일하면 동일한 타입이다.
+
+int sub(int a, int b) { return a - b; } // int(int, int)
+int add(int a, int b) { return a + b; } // int(int, int)
 
 int main()
 {
@@ -14,5 +18,7 @@ int main()
   int *p = &a;
   // add => int(int a, int b)
   int (*p2)(int a, int b) = &add;
+
+  p2 = &sub;
   cout << (*p2)(10, 20) << endl;
 }
