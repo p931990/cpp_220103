cfc94d5a763e06591731faa294d9e09c9736ac95
Chansik Yun <chansigi@gmail.com>
2022-01-04 16:16:36 +0900

    .

--- 220104/10_OOP2.cpp
+++ 220104/10_OOP2.cpp
@@ -7,6 +7,7 @@ using namespace std;
 //  : 스택 - LIFO
 //    큐  - FIFO
 // 1. 전역 변수 / 함수를 이용한 스택
+/*
 int buff[10];
 int idx = 0;
 
@@ -28,4 +29,38 @@ int main()
 
   cout << pop() << endl;
   cout << pop() << endl;
+}
+*/
+
+// 2. Stack 타입을 설계해봅시다.
+struct Stack
+{
+  int buff[10];
+  int idx;
+};
+
+void push(Stack *s, int v)
+{
+  s->buff[(s->idx)++] = v;
+}
+
+int pop(Stack *s)
+{
+  return s->buff[--(s->idx)];
+}
+
+Stack s1;
+int main()
+{
+  Stack s2;
+
+  // 초기화가 필요합니다.
+  s1.idx = 0;
+  s2.idx = 0;
+
+  push(&s1, 10);
+  push(&s2, 20);
+
+  cout << pop(&s1) << endl;
+  cout << pop(&s2) << endl;
 }
