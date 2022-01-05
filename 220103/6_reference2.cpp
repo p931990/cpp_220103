357c3b850aa2dc2e4ac25cd249774628a498a9d4
Chansik Yun <chansigi@gmail.com>
2022-01-05 11:16:45 +0900

    .

--- 220103/6_reference2.cpp
+++ 220103/6_reference2.cpp
@@ -44,7 +44,6 @@ int main()
   int n = 10;
   // inc1(n);
 
-
   inc2(&n);
   inc3(n);
 
@@ -67,6 +66,7 @@ int main()
   int x[5];
   printf("main: sizeof(%ld)\n", sizeof(x));
 
-  foo(x);
-  goo(x);
+  foo(x); // 배열의 첫번째 원소의 시작 주소로 전달됩니다. - 디케이
+  goo(x); // 참조 타입은 디케이가 발생하지 않습니다.
+          // 정확한 타입이 명시되어야 합니다.
 }
